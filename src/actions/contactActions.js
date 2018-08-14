5ef8202e5119bc6cd298399f5eda6f0746dc4ffd
Shashi Kashyap <shashi.aimit@hotmail.com>
2018-08-15 01:20:13 +0530

    POST & DELETE dispatch

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -10,16 +10,27 @@ export const getContacts = () => async dispatch => {
   });
 }
 
-export const addContact = (contactData) => {
-  return {
+export const addContact = (contactData) => async dispatch => {
+  const res = await axios.post('https://jsonplaceholder.typicode.com/users', contactData);
+
+  dispatch({
     type: ADD_CONTACT,
-    payload: contactData
-  };
+    payload: res.data
+  });
 }
 
-export const deleteContact = (id) => {
-  return {
-    type: DELETE_CONTACT,
-    payload: id
-  };
+export const deleteContact = (id) => async dispatch => {
+  try {
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id
+    });
+  } catch(e) {
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id
+    });
+  }
 }
